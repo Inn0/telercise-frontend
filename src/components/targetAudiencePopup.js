676cab5e69e7274792b092301875546354283cd6
Inn0 <daan.brocatus@outlook.com>
2021-05-17 14:40:21 +0200

    fix ta popup

--- src/components/targetAudiencePopup.js
+++ src/components/targetAudiencePopup.js
@@ -1,24 +1,46 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './targetAudiencePopup.css';
 
 function TargetAudiencePopup(props) {
     const [value, setValue] = useState("")
-    const [target, setTarget] = useState("kids")
 
     const handleChange = (e) => {
-        setTarget(e.target.value)
-        setValue(e.target.value)
+        setValue(e.target.value);
         localStorage.setItem("target audience", e.target.value)
     }
+
+    useEffect(() => {
+        if(localStorage.getItem("target audience")){
+            setValue(localStorage.getItem("target audience"))
+        }
+    }, [])
 
     return (
         <div className="popupShadow">
             <div className="popupContainer">
                 <p className="popupTitle">Change target audience</p>
-                <fieldset className="popupRadioButton" value={target} onChange={(event) => handleChange(event)}>
-                <label htmlFor ="kids"><input type="radio" name="target-audience-option" value="kids" id="kids" />Kids</label>
-                <label htmlFor="adults"><input type="radio" name="target-audience-option" value="adult" id="adults" />Adults</label>
-                </fieldset>
+
+                <div className="popupRadioButtonContainer">
+                    <label>
+                        <input
+                            type="radio"
+                            value="adults"
+                            checked={value === "adults"}
+                            onChange={handleChange}
+                        />
+                        Adults
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="kids"
+                            checked={value === "kids"}
+                            onChange={handleChange}
+                        />
+                        Kids
+                    </label>
+                </div>
+
                 <div className="popupButton">
                     <p onClick={() => props.toggle()} className="popupButtonTitle">Save</p>
                 </div>
